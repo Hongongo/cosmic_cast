55d482a41d38a8e16843245a88779198dc2bdbd8
Jose Martínez <jo.mtz.133@gmail.com>
2025-01-25 21:55:31 -0600

    custom app bar added

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:cosmic_cast/presentation/widgets/widgets.dart';
 import 'package:cosmic_cast/presentation/providers/movies/movies_providers.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -38,15 +39,24 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
     if (nowPlayingMovies.isEmpty) return const CircularProgressIndicator();
 
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (BuildContext context, int index) {
-        final movie = nowPlayingMovies[index];
+    return Column(
+      children: [
+        const CustomAppbar(),
 
-        return ListTile(
-          title: Text(movie.title),
-        );
-      },
+        // lista de peliculas
+        Expanded(
+          child: ListView.builder(
+            itemCount: nowPlayingMovies.length,
+            itemBuilder: (BuildContext context, int index) {
+              final movie = nowPlayingMovies[index];
+          
+              return ListTile(
+                title: Text(movie.title),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
