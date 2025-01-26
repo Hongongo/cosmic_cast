858765b7398e4789ae42ef2f6307c7d5a692da82
Jose Martínez <jo.mtz.133@gmail.com>
2025-01-26 04:14:21 -0600

    movie detail model and mapper added

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -1,4 +1,5 @@
 import 'package:cosmic_cast/domain/entities/movie.dart';
+import 'package:cosmic_cast/infrastructure/models/moviedb/movie_details.dart';
 import 'package:cosmic_cast/infrastructure/models/moviedb/movie_moviedb.dart';
 
 class MovieMapper {
@@ -20,6 +21,27 @@ class MovieMapper {
         title: moviedb.title,
         video: moviedb.video,
         voteAverage: moviedb.voteAverage,
+        voteCount: moviedb.voteCount,
+      );
+
+  static Movie movieDetailsToEntity(MovieDetails moviedb) => Movie(
+        adult: moviedb.adult,
+        backdropPath: (moviedb.backdropPath != '')
+            ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
+            : 'https://ih1.redbubble.net/image.5055723050.4010/flat,750x,075,f-pad,750x1000,f8f8f8.jpg',
+        genreIds: moviedb.genres.map((e) => e.name).toList(),
+        id: moviedb.id,
+        originalLanguage: moviedb.originalLanguage,
+        originalTitle: moviedb.originalTitle,
+        overview: moviedb.overview,
+        popularity: moviedb.popularity,
+        posterPath: (moviedb.posterPath != '')
+            ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
+            : 'no-poster',
+        releaseDate: moviedb.releaseDate != '' ? DateTime.parse(moviedb.releaseDate) : DateTime.now(),
+        title: moviedb.title,
+        video: moviedb.video,
+        voteAverage: moviedb.voteAverage.toDouble(),
         voteCount: moviedb.voteCount,
       );
 }
