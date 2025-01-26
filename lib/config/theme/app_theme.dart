6249b942ac0d7646b520321a9cbb16339ca5c18e
Jose Martínez <jo.mtz.133@gmail.com>
2025-01-26 02:50:11 -0600

    color scheme seed changed to purple

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class AppTheme {
   ThemeData getTheme() => ThemeData(
     useMaterial3: true,
-    colorSchemeSeed: const Color.fromARGB(255, 8, 32, 90),
+    colorSchemeSeed: Colors.purple,
   );
 }
