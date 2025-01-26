13b7da279cd57e0c1802d0d67667d7a25cef93e2
Jose Martínez <jo.mtz.133@gmail.com>
2025-01-25 19:27:19 -0600

    response parse when getting now playing

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -1,6 +1,8 @@
 import 'package:cosmic_cast/config/constants/environment.dart';
 import 'package:cosmic_cast/domain/datasources/movies_datasource.dart';
 import 'package:cosmic_cast/domain/entities/movie.dart';
+import 'package:cosmic_cast/infrastructure/mappers/movie_mapper.dart';
+import 'package:cosmic_cast/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:dio/dio.dart';
 
 class MoviedbDatasource extends MoviesDataSource {
@@ -17,7 +19,16 @@ class MoviedbDatasource extends MoviesDataSource {
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
     final response = await dio.get('/movies/now_playing');
-    final List<Movie> movies = [];
+
+    final movieResponse = MovieDbResponse.fromJson(response.data);
+
+    final List<Movie> movies = movieResponse.results
+        .where((moviedb) => moviedb.posterPath != 'no-poster')
+        .map(
+          (moviedb) => MovieMapper.movieDBToEntity(moviedb),
+        )
+        .toList();
+
     return movies;
   }
 }
