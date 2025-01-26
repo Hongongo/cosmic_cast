850f1d641498d3a2604832b148d4669f7b672ae9
Jose Martínez <jo.mtz.133@gmail.com>
2025-01-26 17:17:02 -0600

    actors request to api

--- lib/infrastructure/datasources/actor_moviedb_datasource.dart
+++ lib/infrastructure/datasources/actor_moviedb_datasource.dart
@@ -1,9 +1,32 @@
+import 'package:dio/dio.dart';
+
+import 'package:cosmic_cast/config/constants/environment.dart';
 import 'package:cosmic_cast/domain/datasources/actors_datasource.dart';
 import 'package:cosmic_cast/domain/entities/actor.dart';
+import 'package:cosmic_cast/infrastructure/mappers/actor_mapper.dart';
+import 'package:cosmic_cast/infrastructure/models/moviedb/credits_response.dart';
 
 class ActorMoviedbDataSource extends ActorsDataSource {
+  final dio = Dio(
+    BaseOptions(
+      baseUrl: 'https://api.themoviedb.org/3',
+      queryParameters: {
+        'api_key': Environment.theMovieDbKey,
+        'language': 'en-US',
+      },
+    ),
+  );
+
   @override
-  Future<List<Actor>> getActorsByMovie(String movieId) async{
-    return [];
+  Future<List<Actor>> getActorsByMovie(String movieId) async {
+    final response = await dio.get('/movie/$movieId/credits');
+
+    final castResponse = CreditsResponse.fromJson(response.data);
+
+    List<Actor> actors = castResponse.cast.map(
+      (cast) => ActorMapper.castToEntity(cast),
+    ).toList();
+
+    return actors;
   }
 }
