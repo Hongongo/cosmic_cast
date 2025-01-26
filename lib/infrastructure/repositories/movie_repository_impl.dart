4b6914a93bd33c061884ab5dfc4a0faa04188cdf
Jose Martínez <jo.mtz.133@gmail.com>
2025-01-25 19:32:00 -0600

    movies repository implementation added

---
+++ lib/infrastructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,16 @@
+import 'package:cosmic_cast/domain/datasources/movies_datasource.dart';
+import 'package:cosmic_cast/domain/entities/movie.dart';
+import 'package:cosmic_cast/domain/repositories/movies_repository.dart';
+
+class MovieRepositoryImpl extends MoviesRepository {
+  final MoviesDataSource datasource;
+
+  MovieRepositoryImpl({
+    required this.datasource,
+  });
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) {
+    return datasource.getNowPlaying(page: page);
+  }
+}
