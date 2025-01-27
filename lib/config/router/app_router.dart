8870171f8565475eaa4a3db37f8c07004ae54f4d
Jose Martínez <jo.mtz.133@gmail.com>
2025-01-27 11:27:24 -0600

    home screen refactor to handle home/favorite views

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
 
 import 'package:cosmic_cast/presentation/screens/screens.dart';
+import '../../presentation/views/views.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/',
@@ -9,7 +10,9 @@ final appRouter = GoRouter(
     GoRoute(
         path: '/',
         name: HomeScreen.name,
-        builder: (context, state) => const HomeScreen(),
+        builder: (context, state) => const HomeScreen(
+              childView: HomeView(),
+            ),
         routes: [
           //* movie screen route
           GoRoute(
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,121 +1,22 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'package:cosmic_cast/presentation/providers/providers.dart';
 import 'package:cosmic_cast/presentation/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
 
-  const HomeScreen({super.key});
+  final Widget childView;
+
+  const HomeScreen({
+    super.key,
+    required this.childView,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: _HomeView(),
+    return Scaffold(
+      body: childView,
       bottomNavigationBar: CustomBottomNavigation(),
-    );
-  }
-}
-
-class _HomeView extends ConsumerStatefulWidget {
-  const _HomeView();
-
-  @override
-  _HomeViewState createState() => _HomeViewState();
-}
-
-class _HomeViewState extends ConsumerState<_HomeView> {
-  @override
-  void initState() {
-    super.initState();
-
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-    ref.read(popularMoviesProvider.notifier).loadNextPage();
-    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final initialLoading = ref.watch(initialLoadingProvider);
-    if (initialLoading) return const FullScreenLoader();
-
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    final slideShowMovies = ref.watch(moviesSlideShowProvider);
-    final popularMovies = ref.watch(popularMoviesProvider);
-    final upcomingMovies = ref.watch(upcomingMoviesProvider);
-    final topRatedMovies = ref.watch(topRatedMoviesProvider);
-
-    return CustomScrollView(
-      slivers: [
-        //* sliver app bar
-        const SliverAppBar(
-          floating: true,
-          flexibleSpace: FlexibleSpaceBar(
-            titlePadding: EdgeInsets.all(0),
-            title: CustomAppbar(),
-          ),
-        ),
-
-        //* sliver list
-        SliverList(
-          delegate: SliverChildBuilderDelegate(
-            childCount: 1,
-            (context, index) {
-              return Column(
-                children: [
-                  //* card carousel
-                  MoviesSlideShow(movies: slideShowMovies),
-
-                  //* actualmente
-                  MovieHorizontalListView(
-                    movies: nowPlayingMovies,
-                    title: 'En cines',
-                    subtitle: 'Lunes 20',
-                    loadNextPage: ref
-                        .read(nowPlayingMoviesProvider.notifier)
-                        .loadNextPage,
-                  ),
-
-                  //* proximamente
-                  MovieHorizontalListView(
-                    movies: upcomingMovies,
-                    title: 'Próximamente',
-                    subtitle: 'En este mes',
-                    loadNextPage:
-                        ref.read(upcomingMoviesProvider.notifier).loadNextPage,
-                  ),
-
-                  //* populares
-                  MovieHorizontalListView(
-                    movies: popularMovies,
-                    title: 'Populares',
-                    // subtitle: 'Lunes 20',
-                    loadNextPage:
-                        ref.read(popularMoviesProvider.notifier).loadNextPage,
-                  ),
-
-                  //* mejor calificadas
-                  MovieHorizontalListView(
-                    movies: topRatedMovies,
-                    title: 'Mejor calificadas',
-                    subtitle: 'De todos los tiempos',
-                    loadNextPage:
-                        ref.read(topRatedMoviesProvider.notifier).loadNextPage,
-                  ),
-
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  //
-                ],
-              );
-            },
-          ),
-        ),
-      ],
     );
   }
 }
