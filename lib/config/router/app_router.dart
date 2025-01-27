41bf77b25cd7b2d13cd5c2a9bd69ffd1cebd137b
Jose Martínez <jo.mtz.133@gmail.com>
2025-01-27 12:31:02 -0600

    fix movie screen navigation

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -7,22 +7,29 @@ final appRouter = GoRouter(
   routes: [
     //* home screen route
     GoRoute(
-        path: '/home/:page',
-        name: HomeScreen.name,
-        builder: (context, state) {
-          final pageIndex = int.parse(state.pathParameters['page'] ?? '0');
-          return HomeScreen(pageIndex: pageIndex);
-        },
-        routes: [
-          //* movie screen route
-          GoRoute(
-            path: '/movie/:id',
-            name: MovieScreen.name,
-            builder: (context, state) {
-              final movieID = state.pathParameters['id'] ?? 'no-id';
-              return MovieScreen(movieId: movieID);
-            },
-          ),
-        ]),
+      path: '/home/:page',
+      name: HomeScreen.name,
+      builder: (context, state) {
+        final pageIndex = int.parse(state.pathParameters['page'] ?? '0');
+        return HomeScreen(pageIndex: pageIndex);
+      },
+      routes: [
+        //* movie screen route
+        GoRoute(
+          path: '/movie/:id',
+          name: MovieScreen.name,
+          builder: (context, state) {
+            final movieID = state.pathParameters['id'] ?? 'no-id';
+            return MovieScreen(movieId: movieID);
+          },
+        ),
+      ],
+    ),
+
+    //* redirect route
+    GoRoute(
+      path: '/',
+      redirect: (_, __) => '/home/0',
+    ),
   ],
 );
