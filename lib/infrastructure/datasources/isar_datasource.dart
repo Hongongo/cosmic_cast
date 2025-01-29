3a473e043b2135ee9ece1d311a6ef74287fec155
Jose Martínez <jo.mtz.133@gmail.com>
2025-01-29 13:28:12 -0600

    isar datasource init

--- lib/infrastructure/datasources/isar_datasource.dart
+++ lib/infrastructure/datasources/isar_datasource.dart
@@ -1,9 +1,28 @@
 import 'package:cosmic_cast/domain/datasources/local_storage_datasource.dart';
 import 'package:cosmic_cast/domain/entities/movie.dart';
 import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
 class IsarDatasource extends LocalStorageDataSource {
   late Future<Isar> db;
+
+  IsarDatasource() {
+    db = openDB();
+  }
+
+  Future<Isar> openDB() async {
+    final dir = await getApplicationDocumentsDirectory();
+    if (Isar.instanceNames.isEmpty) {
+      return await Isar.open(
+        [MovieSchema],
+        directory: dir.path,
+        inspector: true,
+      );
+    }
+
+    return Future.value(Isar.getInstance());
+  }
+
   @override
   Future<bool> isMovieFavorite(int movieId) {
     // TODO: implement isMovieFavorite
