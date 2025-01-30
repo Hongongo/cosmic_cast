381d4424afc4e39d46213accd3102e7a6d046866
Jose Martínez <jo.mtz.133@gmail.com>
2025-01-30 00:12:50 -0600

    added favorites view with favorite movies in a masonry gridview

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
-class FavoritesView extends StatelessWidget {
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:cosmic_cast/presentation/providers/providers.dart';
+import 'package:cosmic_cast/presentation/widgets/widgets.dart';
+
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('fav'),);
+    final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
+
+    return MovieMasonry(
+      movies: favoriteMovies,
+    );
   }
 }
