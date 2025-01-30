5ba53759b53059e2a7b9a415bc0deb3ba326992e
Jose Martínez <jo.mtz.133@gmail.com>
2025-01-29 19:26:55 -0600

    added togglefavorite, isMovieFavorite and loadMovies methods to isar datasource

--- lib/infrastructure/datasources/isar_datasource.dart
+++ lib/infrastructure/datasources/isar_datasource.dart
@@ -1,7 +1,8 @@
-import 'package:cosmic_cast/domain/datasources/local_storage_datasource.dart';
-import 'package:cosmic_cast/domain/entities/movie.dart';
 import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
+
+import 'package:cosmic_cast/domain/datasources/local_storage_datasource.dart';
+import 'package:cosmic_cast/domain/entities/movie.dart';
 
 class IsarDatasource extends LocalStorageDataSource {
   late Future<Isar> db;
@@ -24,20 +25,34 @@ class IsarDatasource extends LocalStorageDataSource {
   }
 
   @override
-  Future<bool> isMovieFavorite(int movieId) {
-    // TODO: implement isMovieFavorite
-    throw UnimplementedError();
+  Future<bool> isMovieFavorite(int movieId) async {
+    final isar = await db;
+
+    final Movie? isFavoriteMovie =
+        await isar.movies.filter().idEqualTo(movieId).findFirst();
+
+    return isFavoriteMovie != null;
   }
 
   @override
-  Future<List<Movie>> loadMovies({limit = 10, offset = 0}) {
-    // TODO: implement loadMovies
-    throw UnimplementedError();
+  Future<List<Movie>> loadMovies({limit = 10, offset = 0}) async {
+    final isar = await db;
+
+    return isar.movies.where().offset(offset).limit(limit).findAll();
   }
 
   @override
-  Future<void> toggleFavorite(Movie movie) {
-    // TODO: implement toggleFavorite
-    throw UnimplementedError();
+  Future<void> toggleFavorite(Movie movie) async {
+    final isar = await db;
+
+    final favoriteMovie =
+        await isar.movies.filter().idEqualTo(movie.id).findFirst();
+
+    if (favoriteMovie != null) {
+      return isar
+          .writeTxnSync(() => isar.movies.deleteSync(favoriteMovie.isarId!));
+    }
+
+    return isar.writeTxnSync(() => isar.movies.putSync(movie));
   }
 }
